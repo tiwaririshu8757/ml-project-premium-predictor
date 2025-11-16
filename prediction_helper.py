bce0f58e205067f7ed158e23cfd80cdd19c9d1c2
Rishu kumar <tiwaririshu8757@gmail.com>
2025-11-16 20:02:01 +0530

    updated prediction_helper

--- prediction_helper.py
+++ prediction_helper.py
@@ -1,7 +1,7 @@
 # codebasics ML course: codebasics.io, all rights reserverd
-
-import pandas as pd
 import joblib
+import pandas as pd
+import  joblib
 
 model_young = joblib.load("artifacts\model_young.joblib")
 model_rest = joblib.load("artifacts\model_rest.joblib")
